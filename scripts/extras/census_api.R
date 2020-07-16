699e4fb5f83cb18d79fad99eba2f26f0b6394884
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2020-07-16 09:03:13 -0400

    Name vars and refine multi-year pull

--- scripts/extras/census_api.R
+++ scripts/extras/census_api.R
@@ -13,19 +13,25 @@ library(tidyverse)
 # - Store for future use with `tidycensus::census_api_key()`
 
 
-# Determine vars/tables:
+# Determine tables/vars:
 # - B19013_001: Median household income
 # - B17001_001: Poverty universe
 # - B17001_002: In poverty
 
-vars <- c("B19013_001", "B17001_001", "B17001_002")
+acs_vars <- c(
+  "med_hh_income" = "B19013_001",
+  "pov_univ" = "B17001_001",
+  "in_pov" = "B17001_002"
+)
+
+acs_vars
 
 
 # Pull data (1-year estimates for 2018)
 
 acs_data <- get_acs(
   geography = "state",
-  variables = vars,
+  variables = acs_vars,
   year = 2018,
   survey = "acs1",
   show_call = TRUE
@@ -43,20 +49,22 @@ acs_data %>%
     se = moe / 1.645,
     cv = se / estimate,
     reliable = cv < 0.1
-  ) %>%
-  count(reliable) # All are reliable
+  )
+
+# count(reliable) # All are reliable
 
 
 # Pull multiple years of data
 
-med_hh_income <- map_dfr(
-  2005:2018, function(.x) {
-    
-    get_acs(geography = "us", variables = "B19013_001",
-            year = .x, survey = "acs1") %>%
-      mutate(year = .x, .before = GEOID)
-  }
-)
+get_us_data <- function(vars, year) {
+  get_acs(geography = "us", variables = vars,
+          year = year, survey = "acs1") %>%
+    mutate(year = year, .before = GEOID)
+}
+
+get_us_data("B19013_001", year = 2018) # Works as expected
+
+med_hh_income <- map_dfr(2005:2018, ~ get_us_data("B19013_001", year = .x))
 
 med_hh_income # These figures are not adjusted for inflation
 
